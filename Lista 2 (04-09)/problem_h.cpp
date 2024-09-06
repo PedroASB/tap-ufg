b2f59f7cc06a0c5bcd457778e9c26b5953068142
PedroASB <pedroasbelo@gmail.com>
2024-09-06 20:23:59 -0300

    fix(lista2): problem_h

--- Lista 2 (04-09)/problem_h.cpp
+++ Lista 2 (04-09)/problem_h.cpp
@@ -5,29 +5,30 @@ using namespace std;
 typedef priority_queue<int, vector<int>, greater<int>> min_heap;
 
 int main() {
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int n, number, cost, total_cost;
+    int n, number, total_cost;
+    int aux_1, aux_2; 
     min_heap heap;
 
-    while (cin >> n) {
-        if (n == 0) break;
-
-        cost = total_cost = 0;
+    while (cin >> n, n) {
+        total_cost = 0;
 
         while (n--) {
             cin >> number;
             heap.push(number);
         }
 
-        cost = heap.top();
-        heap.pop();
-
-        while (!heap.empty()) {
-            cost += heap.top();
+        while (true) {
+            aux_1 = heap.top();
             heap.pop();
-            total_cost += cost;
+            if (!heap.empty()) {
+                aux_2 = heap.top();
+                heap.pop();
+                heap.push(aux_1 + aux_2);
+                total_cost += aux_1 + aux_2;
+            } else break;
         }
 
         cout << total_cost << '\n';
