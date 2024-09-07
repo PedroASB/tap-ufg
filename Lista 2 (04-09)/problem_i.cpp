d8bd73e863bc975a32c41e28bf15557017a630a0
PedroASB <pedroasbelo@gmail.com>
2024-09-07 18:37:09 -0300

    fix(lista2): problem_e
    - Problema E passado para problem_e.cpp
    - Tentativa do problema I

---
+++ Lista 2 (04-09)/problem_i.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, l, h, posters, prev_h = -1, last_diff_h = -1, scnd_last_diff_h = -1;
+    cin >> n;
+
+    posters = 0;
+    while (n--) {
+        cin >> l >> h;
+        if (h != prev_h) {
+            scnd_last_diff_h = last_diff_h;
+            last_diff_h = prev_h;
+        }
+
+        if (h > prev_h || (h < prev_h && h != scnd_last_diff_h)) 
+            posters++;
+
+        prev_h = h;
+    }
+
+    cout << posters << '\n';
+
+    return 0;
+}
