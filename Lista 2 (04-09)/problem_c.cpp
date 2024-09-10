77c3286379f2826c6026167fb75fa0f63e623382
PedroASB <pedroasbelo@gmail.com>
2024-09-10 14:09:36 -0300

    feat(lista2): problem_c

--- Lista 2 (04-09)/problem_c.cpp
+++ Lista 2 (04-09)/problem_c.cpp
@@ -6,6 +6,51 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    stack<int> station;
+    queue<int> train;
+    int n, coach, wanted_coach;
+
+    while (cin >> n, n) {
+        while (cin >> coach, coach) {
+            train.push(coach);
+
+            for (int i = 1; i < n; i++) {
+                cin >> coach;
+                train.push(coach);
+            }
+
+            for (int i = 1; i <= n;) {
+                if (train.front() == i) {
+                    train.pop();
+                    i++;
+                } 
+                else if (train.front() < i && !station. empty() && station.top() == train.front()) {
+                    station.pop();
+                    train.pop();
+                } 
+                else {
+                    station.push(i);
+                    i++;
+                }
+            }
+
+            while (!station.empty()) {
+                if (train.front() == station.top()) {
+                    station.pop();
+                    train.pop();
+                } else {
+                    break;
+                }
+            }
+            
+            train.empty() ? cout << "Yes\n" : cout << "No\n";
+
+            while (!train.empty()) train.pop();
+            while (!station.empty()) station.pop();
+        }
+        cout << "\n";
+    }
+
     
     return 0;
 }
